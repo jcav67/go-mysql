81356d864ec789b63c4e192ed68f878d4ef92b87
Camilo Arango <jmilo67@hotmail.com>
2024-04-29 00:10:55 -0500

    :sparkles: get by id and create new contact features added

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jcav67/go-mysql/database"
 	"github.com/jcav67/go-mysql/handlers"
+	"github.com/jcav67/go-mysql/models"
 )
 
 func main() {
@@ -22,8 +23,11 @@ func main() {
 		log.Fatal(err.Error())
 		log.Fatal("ERROR IN DATABASE CONNECTION")
 	}
+	newContact := models.Contact{Name: "usuarioGo2", Email: "go2@go.com", Phone: "123465"}
 
 	handlers.ListarContacts(db)
+	handlers.GetContactById(db, 3)
+	handlers.CreateContact(db, newContact)
 
 	fmt.Println("Conexion a la base de datos exitosa")
 
